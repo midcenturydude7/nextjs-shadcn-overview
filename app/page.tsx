57b764f6313e5dfe7399260268de277ac22ac19b
midCenturyDude7 <mgriffes@gmail.com>
2024-04-14 20:37:43 -0700

    Getting started with shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SearchInput } from "@/components/SearchInput";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex h-screen flex-col items-center justify-center gap-2">
+      <form role="search">
+        <SearchInput />
+      </form>
+      <Button>Submit</Button>
+      <Button size="sm">Submit</Button>
+      <Button size="lg">Submit</Button>
+      <Button size="icon">i</Button>
+      <Button variant="destructive">Submit</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="outline">Submit</Button>
+      <Button variant="secondary">Submit</Button>
+      <Button variant="ghost" size="icon" className="rounded-full">
+        i
+      </Button>
+    </main>
   );
 }
